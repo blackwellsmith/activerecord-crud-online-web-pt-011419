b52dbded74681fc44b0e0dff909bca4aa8e82073
blackwell <blackwellsmithiv@gmail.com>
2019-04-02 15:46:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,6 +27,7 @@ def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
   Movie.create do |m|
     m.title = title
     m.release_date = release_date
+    binding.pry
   end
 end
 
@@ -35,7 +36,7 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-Movie.last.title
+  Movie.last.title
 end
 
 def can_get_size_of_the_database
